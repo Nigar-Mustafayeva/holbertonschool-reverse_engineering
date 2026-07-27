047aff017eda2bc76c8c1929fb6665b9ae3c33d5
Nigar-Mustafayeva <nigar.mustafazade112@gmail.com>
2026-07-27 02:05:06 -0400

    .

--- re_fundamentals/get_entry_point.sh
+++ re_fundamentals/get_entry_point.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-function display_elf_header_info() {
-    echo "ELF Header Information for '$file_name':"
-    echo "----------------------------------------"
-    echo "Magic Number: $magic_number"
-    echo "Class: $class"
-    echo "Byte Order: $byte_order"
-    echo "Entry Point Address: $entry_point_address"
-}
-                                                                                                                                                             
-┌──(kali㉿kali)-[~/Downloads]
-└─$ cat get_entry_point.sh 
-#!/bin/bash
-
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <file>"
     exit 1
